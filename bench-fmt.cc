aa6f4309ce8008f97f5ebcc8e386bfa787faee3b
Michael Clark <michael@metaparadigm.com>
2016-11-05 11:04:21 +1300

    Add string, low precision double and high precision double benchmarks

--- bench-fmt.cc
+++ bench-fmt.cc
@@ -32,26 +32,79 @@ int main()
 {
 	const int ITERS = 100000;
 
+	// snprintf: string %20s
 	{
+		char buf[128];
 		std::chrono::time_point<std::chrono::system_clock> s1 = std::chrono::system_clock::now();
 		for (size_t i = 0; i < ITERS; i ++) {
-			char buf[128];
-			snprintf(buf, sizeof(buf), "%-20s %17.15f\n", "Double 17.15f", 3.141592653589793);
+			snprintf(buf, sizeof(buf), "%-20s\n", "String String");
 		}
 		std::chrono::time_point<std::chrono::system_clock> s2 = std::chrono::system_clock::now();
 		double test_1_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(s2-s1).count() / (double)ITERS;
-		printf("snprintf : %lf nanoseconds\n", test_1_ns);
+		printf("snprintf    (%%20s) : %9.3f nanoseconds\n", test_1_ns);
 	}
 
+	// snprintf: double %7.5f
 	{
+		char buf[128];
 		std::chrono::time_point<std::chrono::system_clock> s1 = std::chrono::system_clock::now();
 		for (size_t i = 0; i < ITERS; i ++) {
-			std::string buf;
-			sprintf(buf, "%-20s %17.15f\n", "Double 17.15f", 3.141592653589793);
+			snprintf(buf, sizeof(buf), "%7.5f\n", 3.141592653589793);
 		}
 		std::chrono::time_point<std::chrono::system_clock> s2 = std::chrono::system_clock::now();
 		double test_1_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(s2-s1).count() / (double)ITERS;
-		printf("c+fmt    : %lf nanoseconds\n", test_1_ns);
+		printf("snprintf   (%%7.5f) : %9.3f nanoseconds\n", test_1_ns);
+	}
+
+	// snprintf: double %17.15f
+	{
+		char buf[128];
+		std::chrono::time_point<std::chrono::system_clock> s1 = std::chrono::system_clock::now();
+		for (size_t i = 0; i < ITERS; i ++) {
+			snprintf(buf, sizeof(buf), "%17.15f\n", 3.141592653589793);
+		}
+		std::chrono::time_point<std::chrono::system_clock> s2 = std::chrono::system_clock::now();
+		double test_1_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(s2-s1).count() / (double)ITERS;
+		printf("snprintf (%%17.15f) : %9.3f nanoseconds\n", test_1_ns);
+	}
+
+	// c+fmt: string %20s
+	{
+		std::string buf;
+		std::chrono::time_point<std::chrono::system_clock> s1 = std::chrono::system_clock::now();
+		for (size_t i = 0; i < ITERS; i ++) {
+			buf.clear();
+			sprintf(buf, "%-20s\n", "String String");
+		}
+		std::chrono::time_point<std::chrono::system_clock> s2 = std::chrono::system_clock::now();
+		double test_1_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(s2-s1).count() / (double)ITERS;
+		printf("c+fmt       (%%20s) : %9.3f nanoseconds\n", test_1_ns);
+	}
+
+	// c+fmt: double %7.5f
+	{
+		std::string buf;
+		std::chrono::time_point<std::chrono::system_clock> s1 = std::chrono::system_clock::now();
+		for (size_t i = 0; i < ITERS; i ++) {
+			buf.clear();
+			sprintf(buf, "%7.5f\n", 3.141592653589793);
+		}
+		std::chrono::time_point<std::chrono::system_clock> s2 = std::chrono::system_clock::now();
+		double test_1_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(s2-s1).count() / (double)ITERS;
+		printf("c+fmt      (%%7.5f) : %9.3f nanoseconds\n", test_1_ns);
+	}
+
+	// c+fmt: double %17.15f
+	{
+		std::string buf;
+		std::chrono::time_point<std::chrono::system_clock> s1 = std::chrono::system_clock::now();
+		for (size_t i = 0; i < ITERS; i ++) {
+			buf.clear();
+			sprintf(buf, "%17.15f\n", 3.141592653589793);
+		}
+		std::chrono::time_point<std::chrono::system_clock> s2 = std::chrono::system_clock::now();
+		double test_1_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(s2-s1).count() / (double)ITERS;
+		printf("c+fmt    (%%17.15f) : %9.3f nanoseconds\n", test_1_ns);
 	}
 
 	return 0;
